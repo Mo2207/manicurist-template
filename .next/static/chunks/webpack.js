d24238002e1eb31228203d28c53d6a4fd1e7125a
Gabe Morris <gabemorris1042@gmail.com>
2025-01-26 12:48:24 -0400

    nav-btn border-radius fix again

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d94bf26226cd930c"; }
+/******/ 		__webpack_require__.h = function() { return "31565ce22dde1cbd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
